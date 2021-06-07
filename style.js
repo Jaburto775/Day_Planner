bc77e607901004cc717977cf4c4fd32c3adbfd23
Justin775 <jaburto775@gmail.com>
2021-06-07 11:49:33 -0500

    variable added to js

---
+++ style.js
@@ -0,0 +1,16 @@
+var today = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+var now = moment().format('h:mm a');
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var sixPm = $("#18pm");
+var sevenPm = $("#19pm");
+var hour = moment().hours();
+var userInput;
+var hourSpan;
