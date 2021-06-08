c4817de001905aa5561cbcbf439d1c0bc8f53caa
Justin775 <jaburto775@gmail.com>
2021-06-08 07:18:07 -0500

    deleted unecessaru variables and added more comments

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 var today = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 var cHour = moment().format('h:mm a');
-var nineAm = $("#9am");
-var tenAm = $("#10am");
-var elevenAm = $("#11am");
-var twelvePm = $("#12pm");
-var onePm = $("#13pm");
-var twoPm = $("#14pm");
-var threePm = $("#15pm");
-var fourPm = $("#16pm");
-var fivePm = $("#17pm");
-var sixPm = $("#18pm");
-var sevenPm = $("#19pm");
 var hour = moment().hours();
 var userInput;
 var hourSpan;
 
+//gets current day
 var interval = setInterval(function() {
   var momentNow = moment();
   $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
@@ -26,9 +16,8 @@ var interval = setInterval(function() {
 function initPage() {
   console.log("Current Hour " + hour);
 } 
-
+// sets times to past present or futur and assigns group/color
 function background () {
-      
   $(".form-control").each(function () {
       var timeTest = parseInt($(this).attr("id"));
       hour = parseInt(hour);
